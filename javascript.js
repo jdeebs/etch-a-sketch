eb0c0ef812e02eea9f4fe1a4ee363ed1ee6eb8db
jdeebs <lftosrs@gmail.com>
2023-04-09 21:46:21 -0700

    Add a hover function to change background color

--- javascript.js
+++ javascript.js
@@ -6,9 +6,22 @@ function makeRows(rows, cols) {
 
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   }
 }
 
 makeRows(16, 16);
+
+function hover() {
+  addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('grid-item')) {
+      event.target.style.backgroundColor = "orange";
+    
+      setTimeout(() => {
+      event.target.style.backgroundColor = "";
+    }, 500);
+  }
+  });
+}
+
+hover();
